91fcd218867b66b40d23b984ce2e67f49304b111
StartForKillerMC <jesussanz2003@gmail.com>
2018-08-20 13:47:00 +0200

    Arreglado comando de exec

--- commands/exec.js
+++ commands/exec.js
@@ -8,16 +8,13 @@ module.exports.run = async (bot, message, args) =>
     var exec = require('child_process').exec;
     //message.delete();
     if (message.author.id !== config.ownerID) return;
-	const code = args[0];
+	const code = args.join("");
 	let executed = exec(code, function (error, stdout, stderr) {
         message.channel.send("STDOUT:");
         message.channel.send('```js\n' + stdout + '```');
         message.channel.send("STDERR:");
         if(stderr) {
             message.channel.send('```js\n' + stderr + '```');
-        }
-        if (error !== null) {
-            message.channel.send('exec error: ' + error);
         }
     });
 }
