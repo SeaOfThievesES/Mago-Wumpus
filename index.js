62b30814c264bf0b936c43c79f412488860ad649
StartForKillerMC <jesussanz2003@gmail.com>
2018-08-20 13:25:31 +0200

    Añadido comando exec

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Client = new Discord.Client();
 const fs = require("fs");
 Client.commands = new Discord.Collection();
 
-this.reloadCmds();
+reloadCmds();
 module.exports.reloadCmds = () => {
     fs.readdir("./commands/", (err, files) =>{
         if(err) console.log(err);
