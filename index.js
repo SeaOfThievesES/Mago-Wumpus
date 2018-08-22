e4bca79ee07d85de7d8a1da87144ee90de636a3f
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-08-22 20:42:45 +0200

    Arreglos en el mensaje de bienvenida

--- index.js
+++ index.js
@@ -68,15 +68,13 @@ Client.on('guildMemberAdd', member => {
     let canalBienvenida = member.guild.channels.find(`name`, "entrada-salida");
     let sinhogarRol = member.guild.roles.find(`name`, "Sinhogar");
     
-    //canalBienvenida.send(`¡Hola ${member}, bienvenido a la Casa de la Brillantez!\nPreséntate en la #salacomún y pide tu rol de la casa mencionando al @Ministerio de la Brillantez.\nhttps://gph.is/2nO4OuI`);
-    
     let bienvenidoEmbed = new Discord.RichEmbed()
     .setTitle("¡Bienvenid@!")
-    .setDescription(`${member} has entrado a la **${member.guild.name}**!\n\nPreséntate en la #salacomún y pide tu rol al Ministerio de la Brillantez.`)
+    .setDescription(`${member.displayName} has entrado a la **${member.guild.name}**.\n\nPreséntate en la #salacomún y pide tu rol al Ministerio de la Brillantez.`)
     .setImage("https://cdn.discordapp.com/attachments/480419711302107136/480788908821577758/2018-08-19_19-22-55.gif")
     .setTimestamp()
     .setColor("#7289da");
-    canalBienvenida.send(bienvenidoEmbed);
+    canalBienvenida.send(member, bienvenidoEmbed);
     member.addRole(sinhogarRol.id);
 });
 
